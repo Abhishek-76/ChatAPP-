7df578d2c19f8b1d596768faae4c31b6ce8be7a8
Abhishek-76 <avinashgautam8923933406@gmail.com>
2023-03-24 23:52:57 +0530

    Sidebar->Search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,13 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <span className="logo">GaamBara Baatchit</span>
+      <span className="logo">GaamBara</span>
       <div className="user">
-        <img src="" alt="" />
-        <span>Abhishek</span>
+        <img
+          src="https://images.pexels.com/photos/7104962/pexels-photo-7104962.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <span>Mahadev</span>
         <button>logout</button>
       </div>
     </div>
