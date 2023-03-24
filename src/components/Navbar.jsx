89768b1548e98b81fb0a4aff388fdd14a5503932
Abhishek-76 <avinashgautam8923933406@gmail.com>
2023-03-24 23:17:01 +0530

    Sidebar->Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <span className="logo">GaamBara Baatchit</span>
+      <div className="user">
+        <img src="" alt="" />
+        <span>Abhishek</span>
+        <button>logout</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
