4e5b08965a19641d106406e2eda26fb395423bd5
Abhishek-76 <avinashgautam8923933406@gmail.com>
2023-03-26 23:22:05 +0530

    Sidebar->Chats->chatsInfo

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 const Chats = () => {
-  return <div className="chats">Chats</div>;
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/13691177/pexels-photo-13691177.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Shri Ram </span>
+          <p>Hello </p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/13691177/pexels-photo-13691177.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Shri Ram </span>
+          <p>Hello </p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/13691177/pexels-photo-13691177.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Shri Ram </span>
+          <p>Hello </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
