0ce8452f7f938476179577f1f398d2e5fa1b60d8
Abhishek-76 <avinashgautam8923933406@gmail.com>
2023-05-04 15:02:29 +0530

    messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/35888/amazing-beautiful-breathtaking-clouds.jpg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img
+          src="https://images.pexels.com/photos/35888/amazing-beautiful-breathtaking-clouds.jpg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
+//message send and recieved
