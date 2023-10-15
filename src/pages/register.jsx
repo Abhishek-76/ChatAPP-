1991311b3e7a8baa55c442e99f3b3bb6eeff1562
Abhishek-76 <avinashgautam8923933406@gmail.com>
2023-10-15 12:00:14 +0530

    dom

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -4,9 +4,12 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db, storage } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -37,6 +40,8 @@ const Register = () => {
               email,
               photoURL: downloadURL,
             });
+            await setDoc(doc(db, "userChats", res.user.uid, {}));
+            navigate("/");
           });
         }
       );
