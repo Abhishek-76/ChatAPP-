fa11c7853a0d2cb31c324fc7b045f655dddc5b73
Abhishek-76 <avinashgautam8923933406@gmail.com>
2023-10-25 20:41:11 +0530

    done with bug update database rules true for all

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -65,7 +65,7 @@ const Register = () => {
         <span className="logo">Lama Chat</span>
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
-          <input required type="text" placeholder="display name" />
+          <input required type="text" placeholder="Name" />
           <input required type="email" placeholder="email" />
           <input required type="password" placeholder="password" />
           <input required style={{ display: "none" }} type="file" id="file" />
@@ -78,7 +78,7 @@ const Register = () => {
           {err && <span>Something went wrong</span>}
         </form>
         <p>
-          You do have an account? <Link to="/register">Login</Link>
+          You do have an account? <Link to="/login">Login</Link>
         </p>
       </div>
     </div>
