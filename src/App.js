e776893544fbb74968c14f1c1eaf931fc8a06a90
Abhishek-76 <avinashgautam8923933406@gmail.com>
2023-10-25 22:27:34 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
 import Register from "./pages/Register";
 import "./style.scss";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
