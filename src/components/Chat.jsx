ac4b4436a4507437daf6472d1e6208186c717d8b
Abhishek-76 <avinashgautam8923933406@gmail.com>
2023-10-25 21:58:09 +0530

    name

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,17 @@
-import React from "react";
 import Cam from "../img/cam.png";
 import Add from "../img/add.png";
 import More from "../img/more.png";
 import Messages from "./Messages";
 import Input from "./Input";
+import DisplayChatName from "./DisplayChatName";
 
 const Chat = () => {
   return (
     <div className="chat">
       <div className="chatInfo">
-        <span>Abhi</span>
+        <span>
+          <DisplayChatName />
+        </span>
         <div className="chatIcons">
           <img src={Cam} alt="" />
           <img src={Add} alt="" />
